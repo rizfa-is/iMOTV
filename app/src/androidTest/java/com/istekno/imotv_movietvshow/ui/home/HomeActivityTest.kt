a6b3345030adc84a4dc58d7ae1c1a6d660feaac8
rizfa-is <67631569+rizfa-is@users.noreply.github.com>
2021-03-10 09:32:52 +0700

    submission 1 completed

--- app/src/androidTest/java/com/istekno/imotv_movietvshow/ui/home/HomeActivityTest.kt
+++ app/src/androidTest/java/com/istekno/imotv_movietvshow/ui/home/HomeActivityTest.kt
@@ -3,12 +3,11 @@ package com.istekno.imotv_movietvshow.ui.home
 import androidx.recyclerview.widget.RecyclerView
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.*
 import com.istekno.imotv_movietvshow.R
-import com.istekno.imotv_movietvshow.ui.splashscreen.SplashScreenActivity
 import com.istekno.imotv_movietvshow.utils.DataDummy
 import org.junit.Before
 import org.junit.Test
@@ -20,7 +19,6 @@ class HomeActivityTest {
 
     @Before
     fun setup() {
-        ActivityScenario.launch(SplashScreenActivity::class.java)
         ActivityScenario.launch(HomeActivity::class.java)
     }
 
@@ -34,17 +32,26 @@ class HomeActivityTest {
     fun loadDetailMovie() {
         onView(withId(R.id.rv_movie)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
 
+        onView(withId(R.id.img_movie)).perform(swipeUp())
+
+        onView(withId(R.id.tv_mov_title)).perform(swipeUp())
         onView(withId(R.id.tv_mov_title)).check(matches(isDisplayed()))
         onView(withId(R.id.tv_mov_title)).check(matches(withText("${dummyMovies[0].title} (${dummyMovies[0].year})")))
 
+        onView(withId(R.id.tv_movie_info)).perform(swipeUp())
         onView(withId(R.id.tv_movie_info)).check(matches(isDisplayed()))
         onView(withId(R.id.tv_movie_info)).check(matches(withText("${dummyMovies[0].country}   |   ${dummyMovies[0].duration}   |   ${dummyMovies[0].rating}")))
 
+        onView(withId(R.id.tv_movie_language)).perform(swipeUp())
         onView(withId(R.id.tv_movie_language)).check(matches(isDisplayed()))
         onView(withId(R.id.tv_movie_language)).check(matches(withText(dummyMovies[0].language)))
 
+        onView(withId(R.id.cg_movie_genre)).perform(swipeUp())
+
+        onView(withId(R.id.tv_overview)).perform(swipeUp())
         onView(withId(R.id.tv_movie_overview)).check(matches(isDisplayed()))
         onView(withId(R.id.tv_movie_overview)).check(matches(withText(dummyMovies[0].overview)))
+
     }
 
     @Test
@@ -54,15 +61,23 @@ class HomeActivityTest {
         onView(withId(R.id.rv_show)).check(matches(isDisplayed()))
         onView(withId(R.id.rv_show)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
 
+        onView(withId(R.id.img_show)).perform(swipeUp())
+
+        onView(withId(R.id.tv_tvs_title)).perform(swipeUp())
         onView(withId(R.id.tv_tvs_title)).check(matches(isDisplayed()))
         onView(withId(R.id.tv_tvs_title)).check(matches(withText("${dummyShows[0].title} (${dummyShows[0].year})")))
 
+        onView(withId(R.id.tv_show_info)).perform(swipeUp())
         onView(withId(R.id.tv_show_info)).check(matches(isDisplayed()))
         onView(withId(R.id.tv_show_info)).check(matches(withText("${dummyShows[0].duration}   |   ${dummyShows[0].rating}")))
 
+        onView(withId(R.id.tv_show_language)).perform(swipeUp())
         onView(withId(R.id.tv_show_language)).check(matches(isDisplayed()))
         onView(withId(R.id.tv_show_language)).check(matches(withText(dummyShows[0].language)))
 
+        onView(withId(R.id.cg_show_genre)).perform(swipeUp())
+
+        onView(withId(R.id.tv_overview)).perform(swipeUp())
         onView(withId(R.id.tv_show_overview)).check(matches(isDisplayed()))
         onView(withId(R.id.tv_show_overview)).check(matches(withText(dummyShows[0].overview)))
     }
